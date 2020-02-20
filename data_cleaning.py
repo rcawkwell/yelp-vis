4bedd611b8558e3f6c46a43f022f70f4c2ab7743
Rebecca Cawkwell <rcawkwell@Rebeccas-MacBook-Pro.local>
2020-02-19 19:30:57 -0500

    data cleaning python file - must access json on own

---
+++ data_cleaning.py
@@ -0,0 +1,28 @@
+import os 
+import json
+
+#load in  photo.json to data_photo and business.json to data_business
+data_photo = []
+with open('photo.json') as f:
+    for line in f:
+        data_photo.append(json.loads(line))
+data_business = []
+with open('business.json') as f:
+    for line in f:
+        data_business.append(json.loads(line))
+
+
+def getData(filename): 
+    for i in data_photo: 
+        if i['photo_id'] == filename: 
+            for j in data_business: 
+                if i['business_id'] == j['business_id']:
+                    #print(j)
+                    return j
+
+directory = os.fsencode("./yelp_photos/photos")
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    file_id, trash = filename.split(".")
+    getData(file_id)
